433f688200190febed6b215cbed34173383c2394
Brad <lazydude33@gmail.com>
2017-04-03 22:04:13 -0600

    final push

--- web/CS313Project2/node-js-getting-started-master/index.js
+++ web/CS313Project2/node-js-getting-started-master/index.js
@@ -1,14 +1,16 @@
+
 var express = require('express');
 var app = express();
 var url = require('url');
 var qs = require('querystring');
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL ||
-'postgres://brother_burton:bradismyfavoritestudent@localhost:5432/tabonus';
+'postgres://brother_burton:bradismyfavoritestudent@localhost:5432/node';
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/public/'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -46,22 +48,27 @@ app.post('/authenticate', function(request, response) {
           var password = POST.password; // $_POST["password"]
 
           const query = client.query(
-            'SELECT id, userName, password, firstName, lastName, isAdmin FROM classUser WHERE username = ($1)', [username]
+            "SELECT id, userName, password, firstName, lastName, isAdmin FROM classUser WHERE userName = 'joe'"
           );
           query.on('row', (row) => { // puts data into rows in results
              results.push(row);
           });
           query.on('end', () => {
              //response.json(results);
+          console.log("finished the query");
+          console.log("Query parameters: " + JSON.stringify(results));
+          // no clue how to pull data out of the results tbh
+          // guessing its something like this:
+          var dbpassword = results.password;
+          var isAdmin    = results.isAdmin;
+          var id         = results.id;
+          var dbuserName = results.userName;
+          var firstName  = results.firstName;
+          var lastName   = results.lastName;
 
-// no clue how to pull data out of the results tbh
-// guessing its something like this:
-// var isAdmin = results.isAdmin;
-// var id = results.is;
-// var userName = results.userName;
-// var firstName = results.firstName;
-// var lastName = results.lastName;
-
+          if(dbpassword != password){
+            response.render('login.html');
+          }
           });
       });
   }
@@ -78,6 +85,56 @@ app.post('/authenticate', function(request, response) {
 }
 );
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.post('/createUser', function(request, response) {
+
+  if (request.method == 'POST') {
+      var body = '';
+      request.on('data', function (data) {
+          body += data;
+          // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+          if (body.length > 1e6) {
+              // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+              request.connection.destroy();
+          }
+      });
+      request.on('end', function() {
+
+          var POST = qs.parse(body);
+          // use POST
+          // when the name of the input text field is "user",
+          // POST.user will show the data of that field.
+          console.log("Query parameters: " + JSON.stringify(POST));
+
+          const client = new pg.Client(connectionString);
+          client.connect();
+
+          const results = []; // creates empty array
+          var username  = POST.usernameC;  // $_POST["username"]
+          var password  = POST.passwordC;  // $_POST["password"]
+          var password2 = POST.passwordC2; // $_POST["password"]
+          var firstName = POST.firstName;  // $_POST["firstName"]
+          var lastName  = POST.lastName;   // $_POST["lastName"]
+          var iNumber   = POST.iNumber;    // $_POST["iNumber"]
+          var isAdmin   = false;
+
+          if(password === password2)
+          {
+            console.log("about to query");
+            const query = client.query(
+              'INSERT INTO classUser (userName, password, firstName, lastName, iNumber, isAdmin) VALUES ($1, $2, $3, $4, $5, $6)',
+              [username, password, firstName, lastName, iNumber, isAdmin]
+            );
+
+      //    query.on('row', (row) => { // puts data into rows in results
+      //       results.push(row);
+      //    });
+          query.on('end', () => {
+
+            response.render('login.html');
+          })
+        }})
+      }});
+
+      app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+      });
